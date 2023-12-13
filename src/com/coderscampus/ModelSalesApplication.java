0d9c1f22358a753e5f10dbbfa635be692bfdfbd1
jeffpodmayer <148505528+jeffpodmayer@users.noreply.github.com>
2023-12-13 11:59:34 -0800

    Refactored with more specific methods

--- src/com/coderscampus/ModelSalesApplication.java
+++ src/com/coderscampus/ModelSalesApplication.java
@@ -1,68 +1,17 @@
 package com.coderscampus;
 
 import java.io.IOException;
-import java.time.YearMonth;
-import java.util.List;
 
 public class ModelSalesApplication {
+	
 
-	public static void main(String[] args) throws IOException {
-		FileService fileService = new FileService();
+	public static void main(String[] args) throws IOException{
 		ModelSalesService modelSalesService = new ModelSalesService();
+	
+		modelSalesService.printYearlySalesReport("Model 3", "model3.csv");
+		modelSalesService.printYearlySalesReport("Model S", "modelS.csv");
+		modelSalesService.printYearlySalesReport("Model X", "modelX.csv");
 		
-		System.out.println("Model 3 Yearly Sales Report");
-		System.out.println("---------------------");
-		
-		List<ModelSales> model3Sales = fileService.getSalesInfoFromCSV("model3.csv");
-		Long model3Sales2017 = modelSalesService.totalNumberOfSalesPerYear(model3Sales, "17");
-		Long model3Sales2018 = modelSalesService.totalNumberOfSalesPerYear(model3Sales, "18");
-		Long model3Sales2019 = modelSalesService.totalNumberOfSalesPerYear(model3Sales, "19");
-		
-		System.out.println("2017 -> " + model3Sales2017); 
-		System.out.println("2018 -> " + model3Sales2018); 
-		System.out.println("2019 -> " + model3Sales2019); 
-		
-		System.out.println("");
-		YearMonth bestModel3Sales = modelSalesService.findBestMonthYearForSales(model3Sales);
-		System.out.println("The best month for Model 3 was: " + bestModel3Sales);
-//		System.out.println("The worst month for Model 3 was: ");//call method here
-//		
-		System.out.println("");
-		System.out.println("Model S Yearly Sales Report");
-		System.out.println("---------------------");
-		
-		List<ModelSales> modelSSales = fileService.getSalesInfoFromCSV("modelS.csv");
-		Long modelSSales2016 = modelSalesService.totalNumberOfSalesPerYear(modelSSales, "16");
-		Long modelSSales2017 = modelSalesService.totalNumberOfSalesPerYear(modelSSales, "17");
-		Long modelSSales2018 = modelSalesService.totalNumberOfSalesPerYear(modelSSales, "18");
-		Long modelSSales2019 = modelSalesService.totalNumberOfSalesPerYear(modelSSales, "19");
-		
-		System.out.println("2016 -> " + modelSSales2016 ); 
-		System.out.println("2017 -> " + modelSSales2017); 
-		System.out.println("2018 -> " + modelSSales2018); 
-		System.out.println("2019 -> " + modelSSales2019); 
-		
-		
-//		System.out.println("The best month for Model S was: ");//call method here
-//		System.out.println("The worst month for Model S was: ");//call method here
-//		
-		System.out.println("");
-		System.out.println("Model X Yearly Sales Report");
-		System.out.println("---------------------");
-		
-		List<ModelSales> modelXSales = fileService.getSalesInfoFromCSV("modelX.csv");
-		Long modelXSales2016 = modelSalesService.totalNumberOfSalesPerYear(modelXSales, "16");
-		Long modelXSales2017 = modelSalesService.totalNumberOfSalesPerYear(modelXSales, "17");
-		Long modelXSales2018 = modelSalesService.totalNumberOfSalesPerYear(modelXSales, "18");
-		Long modelXSales2019 = modelSalesService.totalNumberOfSalesPerYear(modelXSales, "19");
-		
-		System.out.println("2016 -> " + modelXSales2016); 
-		System.out.println("2017 -> " + modelXSales2017); 
-		System.out.println("2018 -> " + modelXSales2018); 
-		System.out.println("2019 -> " + modelXSales2019); 
-		
-//		System.out.println("The best month for Model X was: ");//call method here
-//		System.out.println("The worst month for Model X was: ");//call method here
 	}
 
 }
--- src/com/coderscampus/ModelSalesService.java
+++ src/com/coderscampus/ModelSalesService.java
@@ -1,5 +1,6 @@
 package com.coderscampus;
 
+import java.io.IOException;
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.util.Comparator;
@@ -7,24 +8,61 @@ import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Optional;
 
-
 public class ModelSalesService {
+	FileService fileService = new FileService();
+	ModelSalesService modelSalesService = new ModelSalesService();
 	
 	public Long totalNumberOfSalesPerYear(List<ModelSales> modelSalesList, String year) {
-		IntSummaryStatistics  totalSales = modelSalesList.stream()
-					  									 .filter(modelSales -> modelSales.getMonthYear().contains(year))
-					  									 .mapToInt(modelSales -> Integer.parseInt((modelSales).getSales()))
-					  									 .summaryStatistics();
-			Long yearSales = totalSales.getSum();
-			return yearSales;
+		IntSummaryStatistics totalSales = modelSalesList.stream()
+				.filter(modelSales -> modelSales.getMonthYear().contains(year))
+				.mapToInt(modelSales -> Integer.parseInt((modelSales).getSales())).summaryStatistics();
+
+		Long yearSales = totalSales.getSum();
+		return yearSales;
 	}
-	
+
 	public YearMonth findBestMonthYearForSales(List<ModelSales> modelSalesList) {
 		Optional<ModelSales> bestSalesNum = modelSalesList.stream()
-											 .max(Comparator.comparingInt(modelSales -> Integer.parseInt(modelSales.getSales())));
-		
-		return bestSalesNum.map(modelSales -> YearMonth.parse(modelSales.getMonthYear(),DateTimeFormatter.ofPattern("MMM-yy")))
-						   .orElseThrow(() -> new RuntimeException("No best sales found"));
-		
+				.max(Comparator.comparingInt(modelSales -> Integer.parseInt(modelSales.getSales())));
+
+		return bestSalesNum
+				.map(modelSales -> YearMonth.parse(modelSales.getMonthYear(), DateTimeFormatter.ofPattern("MMM-yy")))
+				.orElseThrow(() -> new RuntimeException("No best sales found"));
 	}
+
+	public YearMonth findWorstMonthYearForSales(List<ModelSales> modelSalesList) {
+		Optional<ModelSales> worstSalesNum = modelSalesList.stream()
+				.min(Comparator.comparingInt(modelSales -> Integer.parseInt(modelSales.getSales())));
+
+		return worstSalesNum
+				.map(modelSales -> YearMonth.parse(modelSales.getMonthYear(), DateTimeFormatter.ofPattern("MMM-yy")))
+				.orElseThrow(() -> new RuntimeException("No best sales found"));
+	}
+
+	public void printYearlySalesReport(String modelName, String fileName) throws IOException {
+		System.out.println(modelName + " Yearly Sales Report");
+		System.out.println("---------------------");
+
+		List<ModelSales> modelSales = fileService.getSalesInfoFromCSV(fileName);
+
+		for (int i = 16; i <= 19; i++) {
+			String year = String.valueOf(i);
+			Long totalSales = modelSalesService.totalNumberOfSalesPerYear(modelSales, year);
+
+			if (totalSales > 0) {
+				System.out.println("20" + year + " -> " + totalSales);
+			}
+		}
+
+		System.out.println("");
+
+		YearMonth bestMonth = modelSalesService.findBestMonthYearForSales(modelSales);
+		YearMonth worstMonth = modelSalesService.findWorstMonthYearForSales(modelSales);
+
+		System.out.println("The BEST month for " + modelName + " was: " + bestMonth);
+		System.out.println("The WORST month for " + modelName + " was: " + worstMonth);
+
+		System.out.println("");
+	}
+
 }
